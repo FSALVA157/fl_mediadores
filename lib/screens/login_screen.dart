929656ed890b53931c2a9390d505f78b8fdfb2ea
xxavierargentino <xxavierargentino@hotmail.com>
2022-10-10 13:59:12 -0300

    working background auth

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,17 +1,24 @@
+import 'package:fl_mediadores_app/utiles/constants.dart';
+import 'package:fl_mediadores_app/widgets/background_auth.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
    
   const LoginScreen({Key? key}) : super(key: key);
 
   static const route = "login";
-  
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-         child: Text('LoginScreen'),
-      ),
+      backgroundColor: kBackgroundColor,
+      
+      body: BackGroundAuth(),
     );
   }
 }
--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,5 +1,5 @@
 export 'package:fl_mediadores_app/screens/home_screen.dart';
 export 'package:fl_mediadores_app/screens/login_screen.dart';
-export 'package:fl_mediadores_app/screens/register_scren.dart';
+export 'package:fl_mediadores_app/screens/register_screen.dart';
 
 
